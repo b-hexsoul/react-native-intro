e2b2e2ffe59d7e43110a0ff766f0ba3e49b10ea0
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-06-09 05:24:38 -0700

    'Init'

---
+++ App.js
@@ -0,0 +1,27 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import Counter from './src/screens/Counter';
+import PictureScreen from './src/screens/PictureScreen';
+import RandomColor from './src/screens/RandomColor';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    ListScreen: ListScreen,
+    Counter: Counter,
+    Pictures: PictureScreen,
+    RandomColor: RandomColor,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
